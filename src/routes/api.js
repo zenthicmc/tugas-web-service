3b036030ac167cb5fd57bad4e4ede22e2e5e8e88
Jagad R R <jagadraya388@gmail.com>
2022-11-03 22:16:51 +0700

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/userController')
+const userController = require('../../src/controllers/UserController')
 
 router.get('/user', userController.show)
 router.get('/user/:id', userController.detail)
